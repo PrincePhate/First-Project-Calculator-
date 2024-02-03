b72c7f3d7bc5add4aa4287f40bbb9690428a50a0
PrincePhate <157995370+PrincePhate@users.noreply.github.com>
2024-02-03 12:20:27 +0200

    Update 1st Project.py
    I refractored my code so that it can be neat and a bit shorter then it was

--- 1st Project.py
+++ 1st Project.py
@@ -1,15 +1,10 @@
-first = float(input("Enter first number => "))
-sec = float(input("Enter second number => "))
+ first, sec=float(input("Enter first number => "))
+sec=float(input("Enter sec number =>"))
 opr = str(input("Enter operation (+, -, *, /) => "))
-
 if opr == "+":
     total = first + sec
-elif opr == "_":
-    total = first - sec
-elif opr == "*":
-    total = first * sec
-elif opr == "/":
-    total = first / sec
+elif opr == "_" "*" "/":
+    total =first - sec,first * sec, first / sec
 else:
     total = str("Please Enter a Valid operation")
 print (total)
